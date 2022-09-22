0afe14927c2edac308829677f2aef8a113764696
gzkdev <ezihegodswill01@gmail.com>
2022-09-22 23:06:27 +0100

    feature: add fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [solution, setSolution] = useState("");
+
+  async function fetchSolutions() {
+    try {
+      let req = await fetch(`http://localhost:3001/solutions`);
+      let res = await req.json();
+      let randomSolution = res[Math.floor(Math.random() * res.length)];
+      setSolution(randomSolution.word);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchSolutions();
+  }, []);
+
   return (
     <div className="App">
       <h1>React Wordle App</h1>
+      {solution && <h3>{solution}</h3>}
     </div>
   );
 }
